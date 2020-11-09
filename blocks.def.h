40fcbe21d52757844cd2f1328e6419cdfb61751c
suassuna <paulo.suassuna@protonmail.com>
2020-11-08 20:16:13 -0500

    add headset battery status

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
-	{"", 		"memory",	30,			0},
-	{"", 		"battery",	5,			0},
-	{"", 		"clock",	5,			0},
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", 		"memory",		30,			0},
+	{"", 		"headsetbattery",	30,			0},
+	{"", 		"battery",		5,			0},
+	{"",		"clock",		5,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
